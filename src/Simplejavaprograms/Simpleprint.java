c15955cd1bf0490964f231bc56a8e9d7dce180bd
keerthana <keer26@gmail.com>
2022-02-16 08:06:36 +0530

    first commit

---
+++ src/Simplejavaprograms/Simpleprint.java
@@ -0,0 +1,14 @@
+package Simplejavaprograms;
+
+public class Simpleprint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("github checking");
+     int x=2;
+     int y=3;
+     
+     System.out.println(x+y);
+	}
+
+}

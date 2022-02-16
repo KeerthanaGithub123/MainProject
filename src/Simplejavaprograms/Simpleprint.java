9d380da1f6904e22611e6bf72b14f2fe33908a7e
keerthana <keer26@gmail.com>
2022-02-16 08:34:39 +0530

    third commit

--- src/Simplejavaprograms/Simpleprint.java
+++ src/Simplejavaprograms/Simpleprint.java
@@ -4,7 +4,7 @@ public class Simpleprint {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.println("github checking");
+     System.out.println("github checking second time");
      int x=2;
      int y=3;
      
